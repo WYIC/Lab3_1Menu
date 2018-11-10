7b812a67c2b6fac454e1e16943918c061cdff32c
WYIC <chiayj-wa16@student.tarc.edu.my>
2018-11-10 14:59:07 +0800

    Initial

---
+++ app/src/main/java/my/edu/tarc/lab3_1menu/AboutUsActivity.java
@@ -0,0 +1,68 @@
+package my.edu.tarc.lab3_1menu;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+
+    public void visitTARUC(View view)
+    {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://www.tarc.edu.my"));
+        if (intent.resolveActivity(getPackageManager()) != null)
+        {
+            startActivity(intent);
+        }
+        else
+        {
+            Toast.makeText(this, "No app too handle this", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void showMain(View view)
+    {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.setData(Uri.parse("https://www.tarc.edu.my"));
+        startActivity(intent);
+    }
+
+    public void showSendto(View view)
+    {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("mailto:" + "info@taruc.edu.my"));
+        if (intent.resolveActivity(getPackageManager()) != null)
+        {
+            startActivity(intent);
+        }
+        else
+        {
+            Toast.makeText(this, "No app too handle this", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void showdial(View view)
+    {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("tel:" + "034123456"));
+        if (intent.resolveActivity(getPackageManager()) != null)
+        {
+            startActivity(intent);
+        }
+        else
+        {
+            Toast.makeText(this, "No app too handle this", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+}
